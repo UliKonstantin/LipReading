c838999b748130dc805513e8854ceabfae893cdc
ronka1 <118555094+ronka1@users.noreply.github.com>
2023-01-21 15:21:50 +0200

    Update dataset.py

--- lipreading/dataset.py
+++ lipreading/dataset.py
@@ -9,7 +9,7 @@ from lipreading.utils import read_txt_lines
 
 
 class MyDataset(object):
-    def __init__(self, modality, data_partition, data_dir, label_fp, annonation_direc=None,
+    def __init__(self, data_partition, data_dir, label_fp, annonation_direc=None,
         preprocessing_func=None, data_suffix='.npz', use_boundary=False):
         assert os.path.isfile( label_fp ), \
             f"File path provided for the labels does not exist. Path iput: {label_fp}."
